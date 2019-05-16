2c93f2b1db636c5f04ea06bb2e19e81ee7643827
onquiera <antoine.onquiert.etu@univ-lille.fr>
2019-05-16 10:54:56 +0300

    script sql separation

--- README_js.md
+++
@@ -1,5 +0,0 @@
-#Auto-compilation
-
-- With the psql-server already launch, we can obtain an auto-compilation of the javascript with this command (executed in `webapp`):
-
-`npm run build`
--- server/src/main/webapp/WEB-INF/classes/mailing/SendMail.java
+++ server/src/main/webapp/WEB-INF/classes/mailing/SendMail.java
@@ -1,6 +1,5 @@
 package mailing;
 
-import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -8,7 +7,6 @@ import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
-import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
